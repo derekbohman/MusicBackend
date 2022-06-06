9c45852621f0e6dac4de58b2f0fb9fe6a3c46d77
Derek Bohman <derekbohman@Dereks-MacBook-Pro.local>
2022-06-06 01:15:22 -0700

    Modified views file to contain get by id function. Also modified the urls file to be able to properly route the http request to get by id.

--- songs/views.py
+++ songs/views.py
@@ -3,17 +3,25 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Song
 from .serializers import SongSerializer
+from songs import serializers
 
 @api_view(['Get', 'POST'])
 def songs_list(request):
-
     if request.method == 'GET':
         songs = Song.objects.all()
         serializer = SongSerializer(songs, many=True)
         return Response(serializer.data)
-
     elif request.method == 'POST':
         serializer = SongSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET'])
+def song_detail(request, pk):
+    try:
+        song = Song.objects.get(pk=pk)
+        serializer = SongSerializer(song)
+        return Response(serializer.data)
+    except Song.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
