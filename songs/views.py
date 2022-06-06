189dad3177cc122d91c8e727ed6dd47bbf1b033e
Derek Bohman <derekbohman@Dereks-MacBook-Pro.local>
2022-06-06 01:32:01 -0700

    Added views functionality of updating database entry with PUT.

--- songs/views.py
+++ songs/views.py
@@ -1,11 +1,11 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Song
 from .serializers import SongSerializer
-from songs import serializers
 
-@api_view(['Get', 'POST'])
+@api_view(['GET', 'POST'])
 def songs_list(request):
     if request.method == 'GET':
         songs = Song.objects.all()
@@ -17,11 +17,14 @@ def songs_list(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT'])
 def song_detail(request, pk):
-    try:
-        song = Song.objects.get(pk=pk)
+    song = get_object_or_404(Song, pk=pk)
+    if request.method == 'GET':
         serializer = SongSerializer(song)
         return Response(serializer.data)
-    except Song.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    elif request.method == 'PUT':
+         serializer = SongSerializer(song, data=request.data)
+         serializer.is_valid(raise_exception=True)
+         serializer.save()
+         return Response(serializer.data)
