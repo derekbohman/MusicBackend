1a4691db3adba05b6ccbc5fb5449d42b2f21da8e
Derek Bohman <derekbohman@Dereks-MacBook-Pro.local>
2022-06-15 10:09:57 -0700

    Finishing touches to backend.

--- music_backend_project/settings.py
+++ music_backend_project/settings.py
@@ -36,7 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'songs'
+    
 ]
 
 MIDDLEWARE = [
@@ -47,7 +49,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'music_backend_project.urls'
 
